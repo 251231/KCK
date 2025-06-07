143dc8d8c0d3292e6e92969c6b2cdc986a5c7971
Lewczuk <252811@edu.p.lodz.pl>
2025-06-07 14:05:05 +0200

    Dodanie klasy MusicManager oraz prostego soundtracku

--- Klasy/main.py
+++ Klasy/main.py
@@ -3,10 +3,24 @@ from StartScreen import StartScreen
 from Game import Game
 from Menu import menu_logowania
 from LoadingScreen import LoadingScreen
-
+from MusicManager import MusicManager
 
 if __name__ == "__main__":
     start_screen = StartScreen()
+    music_manager = MusicManager()
+    
+    # Przykładowe użycie (odkomentuj gdy masz pliki audio)
+    
+    # Ładowanie i odtwarzanie muzyki tła
+    music_manager.load_music("assets/GameDziekanat_sound2.mp3")
+    music_manager.set_music_volume(0.1)
+    music_manager.set_sfx_volume(0.1)
+    music_manager.play_music()
+   
+    
+    
+   
+    
     start_screen.run()
     username = menu_logowania("assets/LoginMenu.png")
     if username:  # Jeśli użytkownik się zalogował
@@ -14,7 +28,11 @@ if __name__ == "__main__":
         loading_screen = LoadingScreen(username)
         if loading_screen.run():  # Jeśli ładowanie zakończone sukcesem
             # Główna gra
+            music_manager.load_music("assets/GameDziekanat_sound1.mp3")
+            music_manager.set_music_volume(0.1)
+            music_manager.set_sfx_volume(0.1)
+            music_manager.play_music()
             game = Game(username)
             game.run()
-    
+    music_manager.cleanup()
     pygame.quit()
